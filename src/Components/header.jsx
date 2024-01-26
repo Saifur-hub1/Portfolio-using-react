564f29ee108ed4dbc11dc4e3a680b5c751970479
Saifur Rahman <sr018695@gmail.com>
2024-01-26 18:52:42 +0600

    nice header done

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,21 +1,40 @@
+import {useEffect} from "react";
 import React from "react";
+
 import '../style/header.css';
-// import helloSvg from '/images/logo/codecave.svg';
+
 const Header = ()=>{
+  useEffect(() => {
+    const handleScroll = () => {
+      let header = document.querySelector("header");
+      if (header) {
+        header.classList.toggle('sticky', window.scrollY > 0);
+      }
+    };
+
+    // Add the event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
   return (
-    <header>
-      <a href="#" className="logo">The Code Cave</a>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/About">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Team</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        </nav>
-    </header>
+      <header className="header">
+        <a href="#" className="logo">The Code Cave</a>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/About">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Team</a></li>
+              <li><a href="#">Contact</a></li>
+          </ul>
+          </nav>
+      </header>
   );
 }
 
